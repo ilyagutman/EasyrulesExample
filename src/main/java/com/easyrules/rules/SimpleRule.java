cc41ba97ae8d0646bd6e9153f6fe3fad520650fd
Ilya Gutman <igutman@hotmail.com>
2015-03-14 22:07:42 -0400

    initial commit

---
+++ src/main/java/com/easyrules/rules/SimpleRule.java
@@ -0,0 +1,31 @@
+package com.easyrules.rules;
+
+import org.easyrules.annotation.Action;
+import org.easyrules.annotation.Condition;
+import org.easyrules.annotation.Rule;
+
+@Rule(name = "Simple Yes/No rule", description = "Prints Yes if rule says yes, and No, if rule evaluates to no.")
+public class SimpleRule {
+
+	/**
+	 * The user input which represents the data that the rule will operate on.
+	 */
+	private String input;
+
+	public SimpleRule(String input) {
+		this.input = input;
+	}
+
+	@Condition
+	public boolean condition() {
+		// The rule should be applied only if
+		// the piece of data reads 'yes'
+		return input.toLowerCase().contains("yes");
+	}
+
+	@Action
+	public void action() throws Exception {
+		System.out.println("the SimpleRule action has fired!");
+	}
+
+}
